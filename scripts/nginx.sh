70cdffbe24aeed832254d29bf64a4ad6dc539567
Alex Olshansky <i@creagenics.com>
2013-06-25 21:49:42 +0400

    Add nginx

---
+++ scripts/nginx.sh
@@ -0,0 +1,5 @@
+echo '-------------------------------------------------------------------------------'
+echo 'Set up nginx'
+echo '------------'
+sudo apt-get install -y nginx
+sudo service nginx start
